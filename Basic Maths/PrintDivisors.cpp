ca428f27109b175987cb964787d1330b001d8c5e
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-10-02 19:26:28 +0530

    Put divisors in a vector and sorted them before the output

--- Basic Maths/PrintDivisors.cpp
+++ Basic Maths/PrintDivisors.cpp
@@ -14,6 +14,7 @@ using namespace std;
 // }  // T.C : O(n)
 
 int main () {
+    vector<int> divisors;
     int n; 
     cout << "num? "; 
     cin >> n;
@@ -21,10 +22,12 @@ int main () {
 
     for (int i = 1; i*i <= n ; i++) {
         if (n % i == 0) {
-            cout << i << endl;
+            divisors.push_back(i);
         }
         if (n / i != i) {
-            cout << n / i << endl;
+            divisors.push_back(n/i);
         }
     }
+    sort (divisors.begin(), divisors.end());
+    for (auto num: divisors) cout << num << endl;
 } // T.C : O(sqrt(n)) ... Because when divisors start repeating when n/i becomes = i .. Tai ektu better O(n) er theke
