765c281bc7846767eb940c54e87303f6a3de58f7
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-10-02 19:46:44 +0530

    A better way to find prime

--- Basic Maths/PrimeNumber.cpp
+++ Basic Maths/PrimeNumber.cpp
@@ -12,3 +12,28 @@ class Solution {
         else return true;
     }
 }; // Inefficient, very inefficient ... what if the number is too large... this is shit af method
+
+
+class Solution {
+  public:
+    bool isPrime(int n) {
+        // code here
+        if (n == 1) return false;
+        if (n == 2) return true;
+        int count = 0;
+        for (int i = 1; i*i <= n; i++) {
+            if (n % i == 0) {
+                count++;
+                if (n/i != i) {
+                    count++;
+                }
+            }   
+            
+        }
+        
+        if (count > 2) return false;
+        else return true;
+    }
+}; // Lot better way ... like O(sqrt(n))    
+
+// gfg te solve kora egulo
