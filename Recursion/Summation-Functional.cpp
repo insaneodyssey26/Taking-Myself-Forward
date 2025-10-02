b8861c7aa5d25e11ec50ceaaafa168d71e77ac7d
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-10-02 22:30:58 +0530

    Summation in functional method -> Without using parameters

---
+++ Recursion/Summation-Functional.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int summation (int n) {
+    if (n == 0) return 0;
+    else return n + summation (n - 1);
+}
+
+int main () {
+    int n ; 
+    cout << "Upto? ";
+    cin >> n;
+    cout << summation(n) << endl;
+}
