249c10ab7ae529a8dd77cec8433a66826881f2f9
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-10-02 20:18:36 +0530

    Euclidean algorithm

--- Basic Maths/GCD.cpp
+++ Basic Maths/GCD.cpp
@@ -14,3 +14,16 @@ class Solution {
     }
 }; // we can calculate gcd like this... but we can see that it's shit again for big values of a, b
 // but still better than classic loop condition... we are performing the loop from reverse direction, so it's lil better
+
+class Solution {
+  public:
+    int gcd(int a, int b) {
+        while (a > 0 && b > 0) {
+            if (a > b) a = a % b; 
+            else b = b % a;
+        }
+        if (a == 0) return b; 
+        else return a;
+    }
+};
+// This is Euclidean algorithm... a better approach to calculate gcd, with lot less time complexity.. something like O(log.min(a,b))
