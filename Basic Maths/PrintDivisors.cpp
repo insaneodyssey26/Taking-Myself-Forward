caa64d611493e7733df341469c4b5fd73eb369d1
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-10-02 19:10:26 +0530

    Print divisors: O(sqrt(n))

--- Basic Maths/PrintDivisors.cpp
+++ Basic Maths/PrintDivisors.cpp
@@ -1,14 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
+// int main () {
+//     int n; 
+//     cout << "number? ";
+//     cin >> n;
+//     cout << "Divisors: " << endl;
+
+//     for (int i = 1; i <= n; i++) {
+//         if (n % i == 0) {
+//             cout << i << endl;
+//         }
+//     }
+// }  // T.C : O(n)
+
 int main () {
     int n; 
-    cout << "number? ";
+    cout << "num? "; 
     cin >> n;
-    cout << "Divisors: " << endl;
-    
-    for (int i = 1; i <= n; i++) {
+    cout << "divisors: " << endl;
+
+    for (int i = 1; i <= n*n ; i++) {
         if (n % i == 0) {
             cout << i << endl;
         }
+        if (n / i == i) {
+            cout << i << endl;
+        }
     }
-}
+} // T.C : O(sqrt(n)) ... Because when divisors start repeating when n/i becomes = i .. Tai ektu better O(n) er theke
