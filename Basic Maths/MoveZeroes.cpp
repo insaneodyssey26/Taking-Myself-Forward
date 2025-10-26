b8ea4dfce9448b38935123531d27ea8b5bd1c619
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-10-26 06:50:43 +0530

    Another method to move zeroes -> This one's optimal as well

--- Basic Maths/MoveZeroes.cpp
+++ Basic Maths/MoveZeroes.cpp
@@ -20,3 +20,20 @@ public:
    // Time complexity: O(n)
    // Space complexity: O(1)
 
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int start = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[start]); 
+                start++; 
+            }
+        }        
+    }
+}; // Another two pointer method, in-place algorithm, this uses swap function
+   // Time complexity: O(n)
+   // Space complexity: O(1)
+   // This method does less write operations than the previous one, so it is better in that sense
+// Both methods are valid and efficient for moving zeroes to the end of the array
