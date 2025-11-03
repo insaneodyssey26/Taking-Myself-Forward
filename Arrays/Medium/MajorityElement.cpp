127cf24cfd2be7a44c965907c898b77504227f8b
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-11-03 21:28:39 +0530

    Majority element

---
+++ Arrays/Medium/MajorityElement.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0, digit = 0;
+        for (int num: nums) {
+            if(count == 0) {
+                digit = num;
+                count++;
+            }
+            else if(num == digit) {
+                count++;
+            }
+            else {
+                count--;
+            }
+        }
+        return digit;
+    }
+};
