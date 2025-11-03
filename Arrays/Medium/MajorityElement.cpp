7e13264a6d0ac79c5cab314fedf37296186c1f95
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-11-03 21:30:15 +0530

    Majority element - gfg version

--- Arrays/Medium/MajorityElement.cpp
+++ Arrays/Medium/MajorityElement.cpp
@@ -1,3 +1,4 @@
+// LeetCode version
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
@@ -16,4 +17,33 @@ public:
         }
         return digit;
     }
+};
+
+// GeeksforGeeks version
+class Solution {
+  public:
+    int majorityElement(vector<int>& arr) {
+        // code here
+        int count = 0, digit = 0;
+        for (int num: arr) {
+            if (count == 0) {
+                digit = num; 
+                count++;
+            }
+            else if (num == digit) {
+                count++;
+            }
+            else {
+                count--;
+            }
+        } // this whole loop finds a candidate for majority element
+        count = 0;
+        for (int num: arr) {
+            if (num == digit) count++;
+        }
+        if (count > (arr.size() / 2)) {
+            return digit;
+        }
+        else return -1;
+    } // this loop verifies if the candidate is actually the majority element
 };
